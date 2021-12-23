64933e229b1b4e329baee37025425c558affaa5a
Laura Train <60850007+lauratrain99@users.noreply.github.com>
2021-12-23 15:46:24 +0100

    attitude dynamics + kinematics implemented

--- aocs_sim.m
+++ aocs_sim.m
@@ -1,22 +1,33 @@
+clear;clc;
 
+% orbital parameter
+mu = 3.986e+5;
+
+% orbit altitude and radius
 h = 3000;
 RE = 6371;
 rmag = RE + h;
-mu = 3.986e+5;
-Torb = 2*pi*sqrt(rmag^3/mu)
+
+% orbital period
+Torb = 2*pi*sqrt(rmag^3/mu);
 
 % assume an Equatorial circular orbit
 r0 = [rmag; 0; 0];
 v0 = [0; sqrt(mu/rmag); 0];
 
+% geometric dimensions of the S/C
 m = 50;
 w = 5;
 h = 6;
 d = 3;
 
+% inertia tensor 
 Isc = [m/12 * (h^2 + d^2), 0, 0; 0, m/12 * (w^2 + d^2), 0; 0, 0, m/12 * (h^2 + w^2)];
 Ix = Isc(1,1);
 Iy = Isc(2,2);
 Iz = Isc(3,3);
 
-
+% attitude initial conditions. 
+wn = 5*pi/Torb;
+w0 = [0.1*wn; 0.1*wn; wn];
+q0 = angle2quat(0,0,pi/2,'ZXZ');
