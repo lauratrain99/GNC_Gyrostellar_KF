f86712ed3605433f1d2ac49f64be86b7cead8984
Laura Train <60850007+lauratrain99@users.noreply.github.com>
2021-12-23 18:47:17 +0100

    magnetic model + reaction wheels torque added

--- aocs_sim.m
+++ aocs_sim.m
@@ -21,7 +21,7 @@ w = 5;
 h = 6;
 d = 3;
 
-% inertia tensor 
+% inertia tensor of the S/C
 Isc = [m/12 * (h^2 + d^2), 0, 0; 0, m/12 * (w^2 + d^2), 0; 0, 0, m/12 * (h^2 + w^2)];
 Ix = Isc(1,1);
 Iy = Isc(2,2);
@@ -31,3 +31,10 @@ Iz = Isc(3,3);
 wn = 5*pi/Torb;
 w0 = [0.1*wn; 0.1*wn; wn];
 q0 = angle2quat(0,0,pi/2,'ZXZ');
+
+% reaction wheels inertial tensor
+Irw = [5.02e-5, 0, 0; 0, 9.41e-5, 0; 0, 0, 5.02e-5];
+
+% magnetic field intensity [nT]
+B0 = 30036.7;
+
