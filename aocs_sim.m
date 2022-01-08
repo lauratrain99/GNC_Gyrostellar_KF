3532cb81a72440c3fe787971db28c5a1d2a40457
Herramar <jmherreramartin@gmail.com>
2022-01-08 18:22:37 +0100

    Bandwith change and visuals

--- aocs_sim.m
+++ aocs_sim.m
@@ -40,17 +40,15 @@ Irw = [5.02e-5, 0, 0; 0, 9.41e-5, 0; 0, 0, 5.02e-5];
 % magnetic field intensity [nT]
 B0 = 30036.7;
 
-
 % Magnetometer
 hyst = (60e-6)*0.001;
-noiseDens = ((50e-12)^2)*334;
+noiseDens = ((50e-12)^2)*40;
 
 %IMU
-noiseDensAcc = 0.33*260*((((0.07/60)/0.01)*10^(-3))^2);
-noiseDensAng =  0.33*260*((0.15/60)^2);
+noiseDensAcc = 0.33*40*((((0.07/60)/0.01)*10^(-3))^2);
+noiseDensAng =  0.33*40*((0.15/60)^2);
 biasAcc = 0.33*((0.00004)^2)/(2*pi);
 biasAng = 0.33*((0.3/3600)^2)/(2*pi);
 
 %StarTracker
-noiseNEA = 0.33*30*((0.55/3600)^2);
-
+noiseNEA = 0.33*10*((0.55*pi/(3600*180))^2);
