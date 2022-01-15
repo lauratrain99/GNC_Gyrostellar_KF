62e41c7efbacec02b68be89c0151751d8f0cbd3e
Herramar <jmherreramartin@gmail.com>
2022-01-15 19:11:26 +0100

    Navigation .m update

--- Navigation Tests/problem1/navigation_1.m
+++ Navigation Tests/problem1/navigation_1.m
@@ -9,14 +9,14 @@ clear;clc;close all;
 addpath ../../Navigation
 
 % Magnetometer
-hyst = (60e-6)*0.001;
-noiseDens = ((50e-12)^2)*40;
+hyst = (60e-6)*0.001; %T
+noiseDens = ((50e-12)^2); %T^2/Hz
 
-% IMU
-noiseDensAcc = 0.33*40*((((0.07/60)/0.01)*10^(-3))^2);
-noiseDensAng =  0.33*40*((0.15/60)^2);
-biasAcc = 0.33*((0.00004)^2)/(2*pi);
-biasAng = 0.33*((0.3/3600)^2)/(2*pi);
+%IMU
+noiseAcc =(0.07/(60*9.81))^2; %g^2/Hz
+noiseAng =(0.15/60)^2; %(deg/s)^2/Hz
+biasAcc = ((0.00004)^2)/(2*pi); %g^2
+biasAng = ((0.3/3600)^2)/(2*pi);%(deg/s)^2
 
-% StarTracker
-noiseNEA = 0.33*10*((0.55*pi/(3600*180))^2);
+%StarTracker
+noiseNEA =(0.55*pi/(3600*180))^2; %rad^2/Hz
