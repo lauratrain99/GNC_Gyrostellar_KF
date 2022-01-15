e21eca4a91a2c63b7ba8985189ee6ecc94b639ab
Herramar <jmherreramartin@gmail.com>
2022-01-16 00:56:14 +0100

    Update nav4

--- Navigation Tests/problem4/navigation_4.m
+++ Navigation Tests/problem4/navigation_4.m
@@ -27,16 +27,16 @@ v0 = [0; sqrt(mu/rmag); 0];
 
 % assume a perturbation a 1% perturbation wrt Z axis angular velocity
 %CASE 1
-wn = 0;
-w0 = [0; 0; wn];
-euler0 =[pi/2, 0, pi/4];
-q0 = angle2quat(euler0(3),euler0(2),euler0(1),'ZYX');
+% wn = 0;
+% w0 = [0; 0; wn];
+% euler0 =[pi/2, 0, pi/4];
+% q0 = angle2quat(euler0(3),euler0(2),euler0(1),'ZYX');
 
 %CASE 2
-%wn = deg2rad(2);
-%w0 = [0; 0; wn];
-% euler0 =[0, 0, 0];
-%q0 = angle2quat(0,0,0,'ZYX');
+wn = deg2rad(2);
+w0 = [0; 0; wn];
+euler0 =[0, 0, 0];
+q0 = angle2quat(0,0,0,'ZYX');
 
 % Geometric and massic properties
 % Iz>Iy, Iz>Ix stable configuration. h>w>d
@@ -61,33 +61,4 @@ biasAng = ((0.3/3600)^2)/(2*pi);%(deg/s)^2
 
 %StarTracker
 noiseNEA =(0.55*pi/(3600*180))^2; %rad^2/Hz
-
-%% 
-% Plot set up
-
-set(groot, 'defaultTextInterpreter',            'latex');
-set(groot, 'defaultAxesTickLabelInterpreter',   'latex'); 
-set(groot, 'defaultLegendInterpreter',          'latex');
-set(groot, 'defaultLegendLocation',             'northeast');
-
-figure()
-plot(out.error_euler.Time, out.error_euler.Data(:,1),'r', ...
-     out.error_euler.Time, out.error_euler.Data(:,2),'b', ...
-     out.error_euler.Time, out.error_euler.Data(:,3),'g')
-title("Navigation solution: error in Euler angles")
-legend("yaw","pitch","roll") 
-xlabel("Time [s]")
-ylabel("Error in Euler angles [deg]")
-grid minor
-
-figure()
-plot(out.error_omega.Time, out.error_omega.Data(:,1),'r', ...
-     out.error_omega.Time, out.error_omega.Data(:,2),'b', ...
-     out.error_omega.Time, out.error_omega.Data(:,3),'g')
-title("Navigation solution: error in angular velocity")
-legend("$\omega_x$","$\omega_y$","$\omega_z$") 
-xlabel("Time [s]")
-ylabel("Error in angular velocity [rad/s]")
-grid minor
-
 
